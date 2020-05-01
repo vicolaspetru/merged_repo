80dfa25d02621c621ba1209d3957bf2d1c0218b3
vicolaspetru <vicolaspetru@gmail.com>
2020-05-01 16:53:09 +0300

    [WIP] Added components css-selectors & colors-palette

---
+++ src/wpzoom-blocks/recipe-card/components/block-settings/shared/colors-palette.js
@@ -0,0 +1,32 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+export const blockColors = [
+    { name: __( 'Dark', 'wpzoom-recipe-card' ), color: '#222222' },
+    { name: __( 'Orange', 'wpzoom-recipe-card' ), color: '#FFA921' },
+    { name: __( 'Red', 'wpzoom-recipe-card' ), color: '#FF4E6A' },
+    { name: __( 'Green', 'wpzoom-recipe-card' ), color: '#B7C662' },
+];
+export const accentColors = [
+    { name: __( 'White', 'wpzoom-recipe-card' ), color: '#FFFFFF' },
+    { name: __( 'Dark', 'wpzoom-recipe-card' ), color: '#333333' },
+];
+export const pinterestBackgroundColors = [
+    { name: __( 'Red', 'wpzoom-recipe-card' ), color: '#C62122' },
+    { name: __( 'Dark', 'wpzoom-recipe-card' ), color: '#222222' },
+    { name: __( 'White', 'wpzoom-recipe-card' ), color: '#FFFFFF' },
+];
+export const pinterestTextColors = [
+    { name: __( 'White', 'wpzoom-recipe-card' ), color: '#FFFFFF' },
+    { name: __( 'Dark', 'wpzoom-recipe-card' ), color: '#333333' },
+];
+export const printBackgroundColors = [
+    { name: __( 'Dark', 'wpzoom-recipe-card' ), color: '#222222' },
+    { name: __( 'Orange', 'wpzoom-recipe-card' ), color: '#FFA921' },
+];
+export const printTextColors = [
+    { name: __( 'White', 'wpzoom-recipe-card' ), color: '#FFFFFF' },
+    { name: __( 'Dark', 'wpzoom-recipe-card' ), color: '#333333' },
+];
