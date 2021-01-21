15a9ee34041ec633ed729d7ef2202958766e4900
vicolaspetru <vicolaspetru@gmail.com>
2021-01-21 12:34:01 +0200

    ❌ Removed folders wpzoom-blocks and structured-data-blocks to use instead folder blocks with server render itself

--- src/wpzoom-blocks/directions/legacy/index.js
+++
@@ -1,5 +0,0 @@
-import v1_0 from './1.0';
-
-export default {
-    v1_0,
-};
--- src/wpzoom-blocks/recipe-card/components/food-labels/library/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Internal dependecies
- */
-export { default as dairyFree } from './dairy-free';
-export { default as glutenFree } from './gluten-free';
-export { default as palmOilFree } from './palm-oil-free';
-export { default as phosphateFree } from './phosphate-free';
-export { default as sugarFree } from './sugar-free';
-export { default as vegan } from './vegan';
